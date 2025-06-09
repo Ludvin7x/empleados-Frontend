7613b0d5c74d9bea001e9b610be9b042ee9c8e7d
Ludvin F. <ludwing.df@live.com>
2025-06-08 20:29:36 -0600

    feat: enhance inicio component with improved layout and Angular Material integration

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';  // Importa Router para la navegación manual
+import { Router } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-inicio',
-  standalone: true,  // Declaración como componente autónomo
+  standalone: true,
+  imports: [MatButtonModule, MatIconModule, LucideAngularModule],
   templateUrl: './inicio.component.html',
 })
 export class InicioComponent {
-  constructor(private router: Router) {}  // Inyecta el servicio Router
+  constructor(private router: Router) {}
 
-  // Función para navegar manualmente
   goTo(path: string): void {
-    this.router.navigate([path]);  // Navega hacia la ruta especificada
+    this.router.navigate([path]);
   }
 }
